decfb665b5e7d9d5c5abb3ff3850780b258ee479
Daniel Connor <danielconnr@gmail.com>
2012-04-04 22:06:17 +0100

    testing

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 var http = require("http"),
 	https = require("https"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs"),
-    argv = process.argv,
-    args = {
+	url = require("url"),
+	path = require("path"),
+	fs = require("fs"),
+	argv = process.argv,
+	args = {
+		baseDomain: ".feedhenry.com",
+		//default domain
 		domain: "apps",
-		app: "KwnzC0J1iaSKWsR9NOS-7D9G",
+		//app guid
+		app: "",
+		//default port
 		port: 8888
 	},
+	//small subset of mime types so to prevent warnings in chrome
 	mimeTypes = {
 		js: "text/javascript",
 		css: "text/css",
 		html: "text/html",
-		xml: "text/xml"
+		xml: "text/xml",
+		txt: "text/plain"
 	};
 
+
+
+//parse the parameters
 for(var i = 0, il = argv.length; i < il; i++) {
 	var param = argv[i].split("=");
 
@@ -24,72 +33,68 @@ for(var i = 0, il = argv.length; i < il; i++) {
 	}
 }
 
-
-var injection = ['<script src="https://' + args.domain + '.feedhenry.com/static/pec/script/studio/155-scripts.js"></script>',
-'<script src="js/stacktrace.js"></script>',
-'<script src="js/EventEmitter.js"></script>',
-'<script src="js/ErrorStore.js"></script>',
-'<script src="js/fh-testing.js"></script>',
-'<script>',
-'        $fhclient = $fh.init({',
-'            "appMode":"debug",',
-'            "checkDeliveryScheme":true,',
-'            "debugCloudType":"fh",',
-'            "debugCloudUrl":',
-'            "https://' + args.domain + '.feedhenry.com",',
-'            "deliveryScheme":"https://",',
-'            "destination":{',
-'                "inline":false,',
-'                "name":"studio"',
-'            },',
-'            "domain": "' + args.domain + '",',
-'            "host": "' + args.domain + '.feedhenry.com",',
-'            "nameserver":"https://ainm.feedhenry.com",',
-'            "releaseCloudType":"fh",',
-'            "releaseCloudUrl":"https://' + args.domain + '.feedhenry.com",',
-'            "swagger_view":"Sju8tJFwM7kox_S1rr1wZ2PS",',
-'            "urltag":"",',
-'            "useSecureConnection":true,',
-'            "user":{',
-'                "id":"YqxcBngHv4nt3j1VstTjQj0X",',
-'                "role":"sub"',
-'            },',
-'            "widget":{',
-'                "guid": "' + args.app + '",',
-'                "inline":false,',
-'                "instance": "' + args.app +'",',
-'                "version":328',
-'            }',
-'        });',
-'</script>'].join('');
-
+var domain = args.domain + args.baseDomain,
+	browserScript = '<script src="https://' + domain + '/static/pec/script/studio/155-scripts.js"></script>',
+	//the device script is loaded from the device because it includes phonegap which the browser does not need
+	deviceScript = '<script src="fhext/js/container.js"></script><script src="js/shake.js"></script><script>window.addEventListener("shake", function() { window.location = "file:///android_asset/www/index.html"}, false);</script>',
+	initialiseScript =  '<script>' +
+	'        $fhclient = $fh.init({' +
+	'            "appMode":"debug",' +
+	'            "checkDeliveryScheme":true,' +
+	'            "debugCloudType":"fh",' +
+	'            "debugCloudUrl":' +
+	'            "https://' + domain + '",' +
+	'            "deliveryScheme":"https://",' +
+	'            "destination":{' +
+	'                "inline":false,' +
+	'                "name":"studio"' +
+	'            },' +
+	'            "domain": "' + args.domain + '",' +
+	'            "host": "' + domain + '",' +
+	'            "nameserver":"https://ainm.feedhenry.com",' +
+	'            "releaseCloudType":"fh",' +
+	'            "releaseCloudUrl":"https://' + domain + '",' +
+	'            "swagger_view":"Sju8tJFwM7kox_S1rr1wZ2PS",' +
+	'            "urltag":"",' +
+	'            "useSecureConnection":true,' +
+	'            "user":{' +
+	'                "id":"YqxcBngHv4nt3j1VstTjQj0X",' +
+	'                "role":"sub"' +
+	'            },' +
+	'            "widget":{' +
+	'                "guid": "' + args.app + '",' +
+	'                "inline":false,' +
+	'                "instance": "' + args.app +'",' +
+	'                "version":328' +
+	'            }' +
+	'        });' +
+	'</script>';
 
 http.createServer(function(request, response) {
 
-	var uri = url.parse(request.url).pathname,
+	var requestParams = url.parse(request.url, true),
+		uri = requestParams.pathname,
 		filename = path.join(process.cwd(), uri),
-		post = [];
+		post = new Buffer(parseInt(request.headers["content-length"], 10)),
+		postLen = 0;
+
+	console.log(filename);
 
 	request.on("data", function(chunk) {
-		post.push(chunk.toString());
+		chunk.copy(post, postLen);
+		postLen += chunk.length;
 	});
 
 	request.on("end", function() {
 		path.exists(filename, function(exists) {
 			if(!exists && uri.indexOf("/box/srv/") !== -1) {
 
-				post = post.join('');
-
 				var proxyReq = https.request({
 					path: request.url,
 					method: request.method,
-					host: args.domain + ".feedhenry.com",
-					headers: {
-						"content-length": post.length,
-						"content-type": "application/json"
-					}
+					host: args.domain + args.baseDomain,
+					headers: request.headers
 				}, function(proxyRes) {
-
 					response.writeHead(proxyRes.statusCode, proxyRes.headers);
 
 					proxyRes.on("data", function(chunk) {
@@ -100,6 +105,7 @@ http.createServer(function(request, response) {
 					});
 				});
 
+
 				proxyReq.write(post);
 
 				proxyReq.end();
@@ -107,8 +113,10 @@ http.createServer(function(request, response) {
 				return;
 			}
 			else if(!exists) {
-				response.writeHead(404,"");
-				response.write("derp");
+				response.writeHead(404,{
+					"Content-Type": mimeTypes.txt
+				});
+				response.write("404 Not found");
 				response.end();
 				return;
 			}
@@ -119,7 +127,7 @@ http.createServer(function(request, response) {
 			fs.readFile(filename, "binary", function(err, file) {
 				if(err) {        
 					response.writeHead(500, {
-						"Content-Type": "text/plain"
+						"Content-Type": mimeTypes.txt
 					});
 					response.write(err + "\n");
 					response.end();
@@ -127,10 +135,12 @@ http.createServer(function(request, response) {
 				}
 
 				response.writeHead(200,{
-					"Content-Type": mimeTypes[filename.split(".").pop()] || "text/plain"
+					//set the response type or default to text/plain
+					"content-type": mimeTypes[filename.split(".").pop()] || mimeTypes.txt
 				});
 				if(filename.indexOf("index.html") > 0) {
-					response.write(injection);
+					//response.write(requestParams.query.device !== undefined ? deviceScript : browserScript);
+					//response.write(initialiseScript);
 				}
 				response.write(file, "binary");
 				response.end();
